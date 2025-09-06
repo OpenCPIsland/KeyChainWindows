930e38956273b72d430f7303dfb3aa1c59b03995
Wednesday Addams <jordanluders@proton.me>
2025-09-06 18:21:59 -0500

    Update dllmain.cpp

--- MemoryMonitorWindows/dllmain.cpp
+++ MemoryMonitorWindows/dllmain.cpp
@@ -1,21 +1,38 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <windows.h>
 #include <psapi.h>
 
-__declspec(dllexport) SIZE_T _getProcessUsedBytes() {
-    HANDLE hProcess;
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the MEMORYMONITORWINDOWS_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// MEMORYMONITORWINDOWS_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef MEMORYMONITORWINDOWS_EXPORTS
+#define MEMORYMONITORWINDOWS_API __declspec(dllexport)
+#else
+#define MEMORYMONITORWINDOWS_API __declspec(dllimport)
+#endif
+
+extern "C" {
+
+// Returns approximate memory usage (Working Set) of the current process
+MEMORYMONITORWINDOWS_API SIZE_T _getProcessUsedBytes() {
+    HANDLE hProcess = GetCurrentProcess();
     PROCESS_MEMORY_COUNTERS pmc;
-    hProcess = GetCurrentProcess();
-    if (K32GetProcessMemoryInfo(hProcess, &pmc, 40)) {
+    if (K32GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
         return pmc.WorkingSetSize;
     }
-    return 0L;
+    return 0;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+} // extern "C"
+
+// Standard DLL entry point
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
@@ -27,4 +44,3 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     }
     return TRUE;
 }
-
